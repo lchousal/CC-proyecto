994ae68593b6c1bd828fdec60adaa031eed1caae
Ernesto Serrano <info@ernesto.es>
2018-01-13 11:43:28 +0100

    Update service.py

--- contenedores/service.py
+++ contenedores/service.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def status_ok():
     return jsonify({'status':'OK'})
 
+@app.route('/status/')
+def status_ok():
+    return jsonify({'status':'OK'})
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
